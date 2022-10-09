2bd48f7c42aaf76b8451422745f135e904840b7f
JIAOZEPENG <115331160+JIAOZEPENG@users.noreply.github.com>
2022-10-09 10:08:16 +0800

    Delete demo09 directory

--- demo09/src/com/buba/java2110a/lipeng/dao/PeopleDao.java
+++
@@ -1,9 +0,0 @@
-package com.buba.java2110a.lipeng.dao;
-
-import com.buba.java2110a.lipeng.entity.People;
-
-import java.util.List;
-
-public interface PeopleDao {
-    List<People> findAll();
-}
--- demo09/src/com/buba/java2110a/lipeng/service/PeopleService.java
+++
@@ -1,9 +0,0 @@
-package com.buba.java2110a.lipeng.service;
-
-import com.buba.java2110a.lipeng.entity.People;
-
-import java.util.List;
-
-public interface PeopleService {
-    List<People> findAll();
-}
